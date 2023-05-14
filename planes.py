dcaa2765e6c9aa76220306be59a6899f85638d93
Alex Villalba <64998363+AlexV11@users.noreply.github.com>
2023-05-14 17:26:38 -0600

    mas cambios

--- planes.py
+++ planes.py
@@ -160,6 +160,65 @@ def app():
     with tab4:
         st.write("Aquí se pueden cambiar planes de estudio existentes")
 
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            df = pd.DataFrame(response.json())
+            df = json_normalize(df.to_dict(orient='records'))
+            df[["descripcion", "nombre"]] = df.apply(extract_desc_name, axis=1)
+            columna_cambiar = df["nombre"]
+            plan_cambio = st.selectbox("Plan de estudio", columna_cambiar, key="plan_cambio")
+            id_plan = df[df["nombre"] == plan_cambio]["id"].values[0]
+
+            url2 = url + "/" + str(id_plan)
+
+            top1, top2 = st.columns(2)
+            mid1, mid2, mid3 = st.columns(3)
+            bot1, bot2, bot3, bot4, bot5 = st.columns(5)
+
+            df_mat = pd.DataFrame(response_mat.json())
+            materias_list = ["descripcion"]
+            df_mat = df_mat[materias_list]
+            df_mat.loc[-1] = [None]
+            materia = top1.selectbox("Materia", df_mat, key="materia_cambio")
+            id_materia = df_mat[df_mat["descripcion"] == materia].index[0] + 1
+    
+            df_carr = pd.DataFrame(response_carr.json())
+            carreras_list = ["nombre"]
+            df_carr = df_carr[carreras_list]
+            carrera = top2.selectbox("Carrera", df_carr, key="carrera_cambio")
+            id_carrera = df_carr[df_carr["nombre"] == carrera].index[0] + 1
+            
+            index = len(df_mat) - 1
+
+            clave_alta = mid1.number_input("Clave del plan de estudio", key="clave_cambio", step=1, format="%i")
+            area_alta = mid2.text_input("Área", key="area_cambio")
+            semestre_alta = mid3.number_input("Semestre", key="semestre_cambio", step=1, format="%i")
+            reqsim = bot1.selectbox("Requisito similar", df_mat, index=index, key="reqsim_cambio")
+            requi1 = bot2.selectbox("Requisito 1", df_mat, index=index, key="requi1_cambio")
+            requi2 = bot3.selectbox("Requisito 2", df_mat, index=index, key="requi2_cambio")
+            requi3 = bot4.selectbox("Requisito 3", df_mat, index=index, key="requi3_cambio")
+            requi4 = bot5.selectbox("Requisito 4", df_mat, index=index, key="requi4_cambio")
+
+            if st.button("Guardar", key = "guardar_plan_cambio"):
+                datos = {
+                    "clave": int(clave_alta),
+                    "idMateria": int(id_materia),
+                    "idCarrera": int(id_carrera),
+                    "fecalt": date.today().isoformat(),
+                    "fecbaj": None,
+                    "area": area_alta,
+                    "reqsim": reqsim,
+                    "requi1": requi1,
+                    "requi2": requi2,
+                    "requi3": requi3,
+                    "requi4": requi4,
+                    "semestre": int(semestre_alta)
+                }
+
+                cambiar_plan(url2, datos)
+                st.experimental_rerun()
+
 def guardar_plan(url, datos):
     response = requests.post(url, json=datos)
 
@@ -180,3 +239,11 @@ def eliminar_plan(url, datos):
         st.success("El plan de estudio se ha eliminado correctamente")
     else:
         st.error("Ha ocurrido un error al eliminar el plan de estudio")
+
+def cambiar_plan(url, datos):
+    response = requests.patch(url, json=datos)
+
+    if response.status_code == 200:
+        st.success("El plan de estudio se ha modificado correctamente")
+    else:
+        st.error("Ha ocurrido un error al modificar el plan de estudio")
